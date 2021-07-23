9fd025edda2639f82e2f0c6e602976e11c0caf57
Yanis Haned <65448727+YanHnd@users.noreply.github.com>
2021-07-23 22:36:03 +0200

    transaction

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    accountName: 'hachibotclient',
-    password: 'Hachibot159',
-    sharedSecret: 'BN1tcG/mDfh9BuiwU6Bg/uTJdwo=',
-}
+  accountName: "hachibotclient",
+  password: "Hachibot159",
+  sharedSecret: "BN1tcG/mDfh9BuiwU6Bg/uTJdwo=",
+  client_id: "43926b5bbe08d3739da3e9b7fb3503536100b6318bfc668a1cc01f306bdeeabb",
+  client_secret:
+    "bf5891d00e8cbc8148c176c995d16975bf204b0770e291e354e2ba76052d606f",
+  redirect_uri: "http://41.109.103.182:3000/userTokenFetch",
+};
--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const SteamUser = require('steam-user')
-const SteamTotp = require('steam-totp')
-const SteamCommunity = require('steamcommunity')
-const TradeOfferManager = require('steam-tradeoffer-manager')
-const moment = require('moment')
+const SteamUser = require("steam-user");
+const SteamTotp = require("steam-totp");
+const SteamCommunity = require("steamcommunity");
+const TradeOfferManager = require("steam-tradeoffer-manager");
+const moment = require("moment");
+const axios = require("axios");
 
-const config = require('./config')
-const answers = require('./messages')
+const config = require("./config");
+const answers = require("./messages");
 
-const { db } = require('./db/dbconfig')
+const { db } = require("./db/dbconfig");
 
 // Actions
-const { getParams } = require('./utilities')
-const { HandlePurchase, HandleSell, HandleDeposit, HandleWithdraw } = require('./actions/transactions')
-const { GetBalance } = require('./actions/userdata')
+const { getParams } = require("./utilities");
+const {
+  HandlePurchase,
+  HandleSell,
+  HandleDeposit,
+  HandleWithdraw,
+} = require("./actions/transactions");
+const { GetBalance } = require("./actions/userdata");
 
-const client = new SteamUser()
-const community = new SteamCommunity()
+const client = new SteamUser();
+const community = new SteamCommunity();
 const manager = new TradeOfferManager({
-    steam: client,
-    community: community,
-    language: 'en',
-})
+  steam: client,
+  community: community,
+  language: "en",
+});
 
 const logOnOptions = {
-    accountName: config.accountName,
-    password: config.password,
-    twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret),
-}
+  accountName: config.accountName,
+  password: config.password,
+  twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret),
+};
 
 // client.on('webSession', (sessionid, cookies) => {
 //     manager.setCookies(cookies)
@@ -35,151 +41,171 @@ const logOnOptions = {
 //     community.startConfirmationChecker(10000, 'your_identity_secret')
 // })
 
-client.logOn(logOnOptions)
+client.logOn(logOnOptions);
 
-client.on('loggedOn', () => {
-    console.log('Hachi bot logged in')
+client.on("loggedOn", () => {
+  console.log("Hachi bot logged in");
 
-    client.enableTwoFactor(() => {})
-    client.setPersona(SteamUser.EPersonaState.Online)
-})
+  client.enableTwoFactor(() => {});
+  client.setPersona(SteamUser.EPersonaState.Online);
+});
 
-client.on('friendsList', () => {})
+client.on("friendsList", () => {});
 
-client.on('friendMessage', async function (steamID, message) {
-    let res = ''
-    switch (true) {
-        case message.includes('!commands'):
-            client.chatMessage(steamID, answers.commands_shortened)
-            break
+client.on("friendMessage", async function (steamID, message) {
+  let res = "";
+  switch (true) {
+    case message.includes("!commands"):
+      client.chatMessage(steamID, answers.commands_shortened);
+      break;
 
-        case message.includes('!how2buy'):
-            client.chatMessage(steamID, answers.how2buy)
-            break
+    case message.includes("!how2buy"):
+      client.chatMessage(steamID, answers.how2buy);
+      break;
 
-        case message.includes('!how2sell'):
-            client.chatMessage(steamID, answers.how2sell)
-            break
+    case message.includes("!how2sell"):
+      client.chatMessage(steamID, answers.how2sell);
+      break;
 
-        case message.includes('!buy'):
-            res = await HandlePurchase(steamID, getParams(message))
-            client.chatMessage(steamID, res.msg)
-            break
+    case message.includes("!buy"):
+      res = await HandlePurchase(steamID, getParams(message));
+      client.chatMessage(steamID, res.msg);
+      break;
 
-        case message.includes('!sell'):
-            res = await HandleSell(steamID, getParams(message))
-            client.chatMessage(steamID, res.msg)
-            break
+    case message.includes("!sell"):
+      res = await HandleSell(steamID, getParams(message));
+      client.chatMessage(steamID, res.msg);
+      break;
 
-        case message.includes('!deposit'):
-            res = await HandleDeposit(steamID, getParams(message))
-            client.chatMessage(steamID, res.msg)
-            break
+    case message.includes("!deposit"):
+      res = await HandleDeposit(steamID, getParams(message));
+      client.chatMessage(steamID, res.msg);
+      break;
 
-        case message.includes('!withdraw'):
-            res = await HandleWithdraw(steamID, getParams(message))
-            client.chatMessage(steamID, res.msg)
-            break
+    case message.includes("!withdraw"):
+      res = await HandleWithdraw(steamID, getParams(message));
+      client.chatMessage(steamID, res.msg);
+      break;
 
-        case message.includes('!prices'):
-            client.chatMessage(steamID, answers.prices)
-            break
+    case message.includes("!prices"):
+      client.chatMessage(steamID, answers.prices);
+      break;
 
-        case message.includes('!fees'):
-            client.chatMessage(steamID, answers.fees)
-            break
+    case message.includes("!fees"):
+      client.chatMessage(steamID, answers.fees);
+      break;
 
-        case message.includes('!mins'):
-            client.chatMessage(steamID, answers.mins)
-            break
+    case message.includes("!mins"):
+      client.chatMessage(steamID, answers.mins);
+      break;
 
-        case message.includes('!balance'):
-            res = await GetBalance(steamID)
-            client.chatMessage(steamID, res)
-            break
+    case message.includes("!balance"):
+      res = await GetBalance(steamID);
+      client.chatMessage(steamID, res);
+      break;
 
-        case message.includes('!stock'):
-            client.chatMessage(steamID, answers.stock)
-            break
-        case message.includes('!stats'):
-            client.chatMessage(steamID, answers.stats)
-            break
+    case message.includes("!stock"):
+      client.chatMessage(steamID, answers.stock);
+      break;
+    case message.includes("!stats"):
+      client.chatMessage(steamID, answers.stats);
+      break;
+  }
+});
+
+client.on("friendRelationship", async (steam_id, relationship) => {
+  console.log("<Friends request from> - ", steam_id.accountid);
+  if (relationship === 2) {
+    let fetch_curr = await db("currencies").select("*");
+
+    console.log(fetch_curr[0]);
+
+    try {
+      db.transaction(async (trx) => {
+        await db("clients")
+          .insert({
+            steam_id: steam_id.accountid,
+            added_date: moment().format("YYYY-MM-DD"),
+          })
+          .transacting(trx);
+
+        await Promise.all(
+          fetch_curr.map(async (elem) => {
+            await db("balances")
+              .insert({
+                id_client: steam_id.accountid,
+                id_currency: elem.id_currency,
+                balance: 0,
+              })
+              .transacting(trx);
+          })
+        );
+      });
+
+      client.addFriend(steam_id);
+      client.chatMessage(steam_id, answers.commands_shortened);
+    } catch (err) {
+      console.log(err);
     }
-})
-
-client.on('friendRelationship', async (steam_id, relationship) => {
-    console.log('<Friends request from> - ', steam_id.accountid)
-    if (relationship === 2) {
-        let fetch_curr = await db('currencies').select('*')
-
-        console.log(fetch_curr[0])
-
-        try {
-            db.transaction(async (trx) => {
-                await db('clients')
-                    .insert({
-                        steam_id: steam_id.accountid,
-                        added_date: moment().format('YYYY-MM-DD'),
-                    })
-                    .transacting(trx)
-
-                await Promise.all(
-                    fetch_curr.map(async (elem) => {
-                        await db('balances')
-                            .insert({
-                                id_client: steam_id.accountid,
-                                id_currency: elem.id_currency,
-                                balance: 0,
-                            })
-                            .transacting(trx)
-                    })
-                )
-            })
-
-            client.addFriend(steam_id)
-            client.chatMessage(steam_id, answers.commands_shortened)
-        } catch (err) {
-            console.log(err)
-        }
-    }
-})
+  }
+});
 
 // Function that sends items
 function sendRandomItem() {
-    manager.loadInventory(440, 2, true, (err, inventory) => {
-        if (err) {
-            console.log(err)
-        } else {
-            const offer = manager.createOffer('partner_steam_id')
-            const item = inventory[Math.floor(Math.random() * inventory.length - 1)]
+  manager.loadInventory(440, 2, true, (err, inventory) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const offer = manager.createOffer("partner_steam_id");
+      const item = inventory[Math.floor(Math.random() * inventory.length - 1)];
 
-            offer.addMyItem(item)
-            offer.setMessage(`Lucky you! You get a ${item.name}!`)
-            offer.send((err, status) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(`Sent offer. Status: ${status}.`)
-                }
-            })
+      offer.addMyItem(item);
+      offer.setMessage(`Lucky you! You get a ${item.name}!`);
+      offer.send((err, status) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Sent offer. Status: ${status}.`);
         }
-    })
+      });
+    }
+  });
 }
 
 // API
-const express = require('express')
-const path = require('path')
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/userTokenFetch', function (req, res) {
-    const { code, state } = req.query
+app.get("/", (req, res) => {
+  console.log("here");
+});
 
-    console.log(req.query)
-})
+app.get("/userTokenFetch", function (req, res) {
+  console.log("here");
+  const { code, state } = req.query;
+
+  axios
+    .post("https://api.coinbase.com/oauth/token", {
+      grant_type: "authorization_code",
+      code: code,
+      client_id: config.client_id,
+      client_secret: config.client_secret,
+      redirect_uri: config.redirect_uri,
+    })
+    .then((res) => {
+      console.log(res.data.access_token);
+    })
+    .catch((e) => {
+      // console.log(e);
+    });
+
+  console.log(req.query);
+});
 
 app.listen(PORT, function () {
-    console.log('Server listenning on port ' + PORT)
-})
+  console.log("Server listenning on port " + PORT);
+});
 
-module.exports.client = client
+module.exports.client = client;
